ab2bd0c8a8919695fe90c602645efc6d7d100918
Natalia Bulbuk <nbulbuk@gmail.com>
2022-12-25 23:35:40 +0000

    add homework 03

---
+++ 03/hmw-03.py
@@ -0,0 +1,35 @@
+# task 1
+print("I love Python " * 42)
+
+# task 2
+age_in_month = 42 * 12 + 11
+print(age_in_month)
+
+# task 3
+age_in_years = age_in_month // 12
+print(age_in_years)
+
+# task 4
+
+my_name = 'Natalia'
+my_age = "My name is " + str(my_name) + ". I'm " + str(age_in_years) + " years old"
+print(my_age)
+
+# task 5
+
+i = 1
+
+print(i, '> 0  - ', i > 0)
+print(i, '!= 1000 - ', i != 1000)
+print(i ** 10, ' < 100 - ', i ** 10 < 100)
+print(i, ' == a  - ', i == 'a')
+print(str(i), ' >= 1 - ', str(i) >= '1')
+
+# task 6
+
+a = 2
+b = 5
+c = 6
+
+d = int(str(a) + str(b) + str(c))
+print(d)
