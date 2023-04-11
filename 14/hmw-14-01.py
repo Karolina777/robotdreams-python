aef82f74006917d284969d15c0ce09f2add1607b
Natalia Bulbuk <nbulbuk@gmail.com>
2023-04-11 19:22:51 +0100

    1st problem of homework 14 with edits

--- 14/hmw-14-01.py
+++ 14/hmw-14-01.py
@@ -7,7 +7,9 @@ import re
 phone_book = dict()
 
 # Regular expression to match phone numbers
-phone_number_pattern = r"^(?:\+?38)?(?:0|\(0\))?\d{9}$"
+phone_number_pattern = r"(\s|^)(?:\+?380|0)\d{9}(\s|$)"
+
+
 
 # create the initial phone book
 while True:
@@ -20,7 +22,7 @@ while True:
         case 1:
             match user_input_split[0]:
                 case 'stop':
-                    print('Phone boom=k is created')
+                    print('Phone book is created')
                     break
                 case _:
                     print('Unknown command, try again')
@@ -42,3 +44,55 @@ while True:
             print('Unknown command, try again')
 
 print(phone_book)
+
+# work with created phone book
+while True:
+    user_input = input('\nWhat do you want to do with your phone book?'
+                       '\nEnter command from the list (stats, add <name phone>, delete <name>, list, show <name>, or stop to finish): ')
+
+    user_input_split = user_input.split()
+    command_len = len(user_input_split)
+
+    match command_len:
+        case 1:
+            match user_input_split[0]:
+                case 'stats':
+                    print(f'Your phone book has {len(phone_book)} records')
+                case 'list':
+                    for key in phone_book.keys():
+                        print(key)
+                case 'stop':
+                    print('Have a nice day!')
+                    break
+                case _:
+                    print('Unknown command, try again')
+        case 2:
+            match user_input_split[0]:
+                case 'delete':
+                    if user_input_split[1] in phone_book.keys():
+                        del phone_book[user_input_split[1]]
+                        print(f'The record with the name {user_input_split[1]} is deleted from phone book')
+                    else:
+                        print(f' The name \'{user_input_split[1]}\' is NOT in your phone book')
+                case 'show':
+                    if user_input_split[1] in phone_book.keys():
+                        print(f'Name is {user_input_split[1]} and phone number is {phone_book[user_input_split[1]]}')
+                    else:
+                        print(f' The name \'{user_input_split[1]}\' is NOT in your phone book')
+                case _:
+                    print('Unknown command, try again')
+        case 3:
+            if user_input_split[0] == 'add':
+                match = re.match(phone_number_pattern, user_input_split[2])
+
+                if not match:
+                    print(f"Number {user_input_split[2]} is not Ukrainian valid number")
+                    continue
+
+                if user_input_split[1] not in phone_book.keys():
+                    phone_book.update({user_input_split[1]: user_input_split[2]})
+                    print(f'Record with the Name {user_input_split[1]} is added to phone book')
+                else:
+                    print(f' The name \'{user_input_split[1]}\' is already in your phone book')
+            else:
+                print('Unknown command, try again')
